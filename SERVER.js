21b8cc43817123e3a1837247919801e1a3d27af1
atuanho2204 <itto22041999@gmail.com>
2020-02-29 21:35:26 -0500

    Demochat

---
+++ SERVER.js
@@ -0,0 +1,67 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+var listUser=[];
+var listMessage=[];
+var listID = [];
+io.on("connection", function(socket) {
+  console.log("Someone just connected" + socket.id);
+  listID.push(socket.id);
+
+  socket.on("client-send-Username", function(data) {
+    console.log(data);
+    if(listUser.indexOf(data)>=0) {
+        socket.emit("server-send-false");
+    }else{
+        listUser.push(data);
+        socket.Username = data;
+        socket.emit("server-send-success", data);
+        io.sockets.emit("server-send-listUser", listUser);
+    }
+  });
+
+  socket.on("logOut", function() {
+    console.log("someonelogout");
+    listUser.splice(
+        listUser.indexOf(socket.Username), 1
+    );
+    socket.broadcast.emit("server-send-listUser", listUser);
+  });
+
+  socket.on("disconnect", function() {
+    console.log("someonelogout");
+    listUser.splice(
+        listUser.indexOf(socket.Username), 1
+    );
+    socket.broadcast.emit("server-send-listUser", listUser);
+  });
+
+  socket.on("client-send-message", function(data) {
+      listMessage.push({user: socket.Username, message:data});
+      io.sockets.emit("server-send-listMessage", listMessage);
+  });
+
+  socket.on("someone-is-texting", function() {
+      //console.log(socket.Username + " .... ");
+      var s = socket.Username + "......"
+      socket.broadcast.emit("server-someoneTexting", s);
+
+  });
+
+  socket.on("someone-stop-texting", function() {
+      console.log(socket.Username + " stop ");
+      io.sockets.emit("server-someoneStopTexting");
+  });
+
+});
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
---
+++ public/app.js
@@ -0,0 +1,63 @@
+var socket= io("http://localhost:3000");
+
+socket.on("server-send-false", function() {
+    alert("Someone has taken this username. Please try again!!!");
+});
+socket.on("server-send-success", function(data) {
+    $("#currentUser").html(data);
+    $("#loginForm").hide(2000);
+    $("#chatForm").show();
+});
+
+socket.on("server-send-listUser", function(data) {
+    $("#boxContent").html("");
+    data.forEach(function(i) {
+        $("#boxContent").append("<div class='user'>" + i + "</div>");
+    });
+});
+
+socket.on("server-send-listMessage", function(data) {
+    var update = data[data.length - 1];
+      $("#listMessages").append(
+        "<div class='message'>" + update.user + ": " + update.message + "</div>"
+      );
+});
+
+socket.on("server-someoneTexting", function(data) {
+    $("#someoneTexting").html(data)
+});
+
+socket.on("server-someoneStopTexting", function(data) {
+    $("#someoneTexting").html("")
+});
+
+$(document).ready(function() {
+    $("#loginForm").show();
+    $("#chatForm").hide();
+
+    $("#btnRegister").click(function() {
+        socket.emit("client-send-Username", $("#txtUserName").val());
+    });
+
+    $("#btnLogout").click(function() {
+        console.log("hello");
+        socket.emit("logOut");
+        $("#loginForm").show(2000);
+        $("#chatForm").hide();
+    });
+
+    $("#btnSendMessage").click(function() {
+        socket.emit("client-send-message", $("#txtMessage").val());
+        $("#txtMessage").val("");
+    });
+
+    $("#txtMessage").focusin(function() {
+        socket.emit("someone-is-texting");
+    });
+
+    $("#txtMessage").focusout(function() {
+        socket.emit("someone-stop-texting");
+    });
+
+
+});
